e0d7cf8784600f352385211c3c89b9966519d06f
Dennis Mbithi <denmbithi@gmail.com>
2018-04-15 13:04:41 +0300

    Created model for Post

--- trydjango/blog/admin.py
+++ trydjango/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'timestamp', 'updated']
+    list_filter = ('title', 'timestamp')
+    search_fields = ('title',)
+
+
+admin.site.register(Post, PostAdmin)
